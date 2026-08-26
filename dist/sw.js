2c898ccf09880cdd0b71bf6a037ebbf787172e92
amnhmb <thirtyonelab.catalog@gmail.com>
2026-08-26 13:02:28 +0800

    fix bugs

--- dist/sw.js
+++ dist/sw.js
@@ -102,7 +102,7 @@ define(['./workbox-619edae6'], (function (workbox) { 'use strict';
     "revision": "23b71e4960bd29ffefb0c6127ab0e8b7"
   }, {
     "url": "index.html",
-    "revision": "f257cc2b32645c15102bdb2f2f420c08"
+    "revision": "f8e00330f843cb5561b7c68d50dbd127"
   }, {
     "url": "contact.html",
     "revision": "a04993137c554aeb4312d01471041a53"
@@ -2405,7 +2405,7 @@ define(['./workbox-619edae6'], (function (workbox) { 'use strict';
     "url": "assets/modulepreload-polyfill-Dezn_h7o.js",
     "revision": null
   }, {
-    "url": "assets/main-DSUPDw_z.js",
+    "url": "assets/main-uVFWpt93.js",
     "revision": null
   }, {
     "url": "assets/main-DOf6Ij4d.css",
